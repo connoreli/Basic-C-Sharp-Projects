34f9dbf043c23acc79b9bc7295f19193451007a9
Connor Eliason <connor.eliason@gmail.com>
2022-05-26 12:35:27 -0600

    ...

---
+++ Basic_C#_Programs/SampleApplication.cs
@@ -0,0 +1,10 @@
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Welcome to Acme Accounting Systems" \n "Remember we're \"Accounting\" on you");
+        Console.ReadLine();
+    }
+}

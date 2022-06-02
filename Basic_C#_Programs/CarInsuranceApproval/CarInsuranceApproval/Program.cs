2d5acfebdf32e902165975ceb2baf5f0aec38cd8
Connor Eliason <connor.eliason@gmail.com>
2022-06-02 13:41:28 -0600

    ..

---
+++ Basic_C#_Programs/CarInsuranceApproval/CarInsuranceApproval/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace CarInsuranceApproval
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your Age?");
+            int age = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Have you ever had a DUI?");
+            bool hadDUI = Convert.ToBoolean(Console.ReadLine());
+
+            Console.WriteLine("How many speeding Tickets do you have?");
+            int speedingTickets = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Qualified?");
+            bool qualified = (age > 15 && hadDUI == false && speedingTickets < 3);
+            Console.WriteLine(qualified);
+            Console.ReadLine();
+        }
+    }
+}

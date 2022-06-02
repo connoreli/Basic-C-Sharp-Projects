5364807e91c29db57105ccbd407d2da223cc9328
Connor Eliason <connor.eliason@gmail.com>
2022-06-02 13:09:45 -0600

    ...

---
+++ Basic_C#_Programs/IncomeComparison/IncomeComparison/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace IncomeComparison
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Anonymous Income Comparison Program");
+
+            Console.WriteLine("Person 1");
+            Console.WriteLine("Hourly rate?");
+            int hourlyRate = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Hours worked per week?");
+            int hoursWorked = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Person 2");
+            Console.WriteLine("Hourly rate?");
+            int hourlyRate2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Hours worked per week?");
+            int hoursWorked2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Annual salary of Person 1:");
+            int salary1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Annual salary of Person 2:");
+            int salary2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Does Person 1 make more money than Person 2?");
+            bool moreMoney = salary1 > salary2;
+            Console.WriteLine(moreMoney.ToString());
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Basic_C#_Programs/MathAndComparisonOperators/MathAndComparisonOperators/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace MathAndComparisonOperators
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int total = 5 + 10;
+            int otherTotal = 12 + 22;
+            int combined = total + otherTotal;
+            Console.WriteLine(combined);
+            Console.ReadLine();
+        }
+    }
+}

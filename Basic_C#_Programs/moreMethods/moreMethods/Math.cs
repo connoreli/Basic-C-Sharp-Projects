f9746560984b5b5e106fa280fb080ff388a9180b
Connor Eliason <connor.eliason@gmail.com>
2022-06-08 16:28:12 -0600

    ...

---
+++ Basic_C#_Programs/moreMethods/moreMethods/Math.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace moreMethods
+{
+    public class Math
+    {
+        public int myMethod(int theirNumber, int optionalNumber = 1)
+        {
+            return theirNumber + optionalNumber;
+        }
+
+    }
+}
--- Basic_C#_Programs/moreMethods/moreMethods/Program.cs
+++ Basic_C#_Programs/moreMethods/moreMethods/Program.cs
@@ -6,7 +6,15 @@ namespace moreMethods
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Math math = new Math();
+
+            Console.WriteLine("Please Enter two numbers, one at a time.");
+            int theirNumber = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("This one is optional.");
+            int optionalNumber = Convert.ToInt32(Console.ReadLine());
+
+            int addition = math.myMethod(theirNumber, optionalNumber = 1);
+            Console.WriteLine(addition);
         }
     }
 }

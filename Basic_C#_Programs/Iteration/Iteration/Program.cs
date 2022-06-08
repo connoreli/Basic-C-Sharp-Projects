d5e820e911b1e375387ccc3fa18d3c1e562c24ec
Connor Eliason <connor.eliason@gmail.com>
2022-06-07 22:58:49 -0600

    console app

---
+++ Basic_C#_Programs/Iteration/Iteration/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[] testscores = { 99, 98, 85, 70, 82, 34, 91, 90, 94 };
+
+        for (int i = 0; i < testscores.Length; i++)
+        {
+            if (testscores[i] > 85)
+            {
+                Console.WriteLine("passing test score: " + testscores[i]);
+            }
+        }
+        Console.ReadLine();
+
+
+        string[] names = { "connor", "dallin", "cody", "haley" };
+
+        for (int n = 0; n < names.Length; n++)
+        {
+            if (names[n] == "connor")
+            {
+                Console.WriteLine(names[n]);
+            }
+        }
+        Console.ReadLine();
+
+        List<int> testscores1 = new List<int>();
+        testscores1.Add(98);
+        testscores1.Add(99);
+        testscores1.Add(81);
+        testscores1.Add(72);
+        testscores1.Add(70);
+
+        foreach (int score in testscores1)
+        {
+            if (score > 85)
+            {
+                Console.WriteLine("passing test score:" + score);
+            }
+        }
+        Console.ReadLine();
+
+        List<string> names1 = new list<string>() { "connor", "olivia", "dallin", "haley" };
+
+        foreach (string name in names1)
+        {
+            console.writeline(name);
+        }
+        console.readline();
+
+        List<int> testScores2 = new List<int>() { 98, 99, 12, 72, 23, 99 };
+        List<int> passingScores = new List<int>();
+
+        foreach (int score in testScores2)
+        {
+            if (score > 85)
+            {
+                passingScores.Add(score);
+            }
+        }
+        Console.WriteLine(passingScores.Count);
+        Console.ReadLine();
+    }
+}
---
+++ Basic_C#_Programs/sixPartAssignment/sixPartAssignment/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace sixPartAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] names = { "Cody", "Dallin", "Haley", "Connor" }; // start of step 1
+            Console.WriteLine("What is a good last name?");
+            string lastNames = Console.ReadLine();
+
+            List<string> list = new List<string>(names.ToList());
+            list.Add(lastNames);
+            names = list.ToArray();
+            foreach (string name in names)
+            {
+                Console.WriteLine(name);
+            }
+
+            list.ForEach(Console.WriteLine);  // end of step 1
+
+
+            for (int i = 0; i < 5; i++)  // infinite loop and loop using <
+            {
+                Console.WriteLine(i);
+            }
+
+            for (int f = 0; f <= 10; f++) // loop using <=
+            {
+                Console.WriteLine(f);
+            }
+
+            List<string> Names = new List<string>() { "CONNOR", "DALLIN", "HALEY" };  // step 4
+            Console.WriteLine(Names);
+            Console.WriteLine("Which name would you like to search for?");
+            string search = Console.ReadLine();
+
+            for (int n = 0; n < Names.Count; n++)
+            {
+                if (Names[n] == "CONNOR")
+                {
+                    Console.WriteLine(Names[0]);
+                    break;
+                }
+                else if (Names[n] == "DALLIN")
+                {
+                    Console.WriteLine(Names[1]);
+                    break;
+                }
+                else if (Names[n] == "HALEY")
+                {
+                    Console.WriteLine(Names[2]);
+                    break;
+                }
+                else Console.WriteLine("This name does not exist.");
+            }
+
+            // step 5
+
+            List<string> Strings = new List<string>() { "Connor", "Bennett", "Ryan", "Seth", "Seth" };
+            Console.WriteLine(Strings);
+            Console.WriteLine("Search for a user!");
+            string Search = Console.ReadLine();
+            foreach (string tring in Strings)
+
+            Console.ReadLine();
+        } 
+        
+    }
+}

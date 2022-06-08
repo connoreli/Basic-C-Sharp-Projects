31759b0defbea8d71e1ff1ab969c9affc35582e1
Connor Eliason <connor.eliason@gmail.com>
2022-06-08 16:04:54 -0600

    update

--- Basic_C#_Programs/Methods/Methods/Program.cs
+++ Basic_C#_Programs/Methods/Methods/Program.cs
@@ -7,7 +7,6 @@ namespace Methods
         static void Main(string[] args)
         {
             Math math = new Math();
-            int secretNumber = 4;
             Console.WriteLine("Pick a number 1-100!");
             int theirNumber = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine("Your number will now be multiplied, added, and subtracted by our secret number!");

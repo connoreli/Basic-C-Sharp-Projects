de46de6fad7dd15bdae7bf469b526611bce2bbb0
Connor Eliason <connor.eliason@gmail.com>
2022-06-08 16:03:15 -0600

    one parameter

--- Basic_C#_Programs/Methods/Methods/Program.cs
+++ Basic_C#_Programs/Methods/Methods/Program.cs
@@ -12,9 +12,9 @@ namespace Methods
             int theirNumber = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine("Your number will now be multiplied, added, and subtracted by our secret number!");
 
-            int Addition = math.Add(theirNumber, secretNumber); 
-            int Subtraction = math.Subtract(theirNumber, secretNumber);
-            int Multiplication = math.Multiply(theirNumber, secretNumber);
+            int Addition = math.Add(theirNumber); 
+            int Subtraction = math.Subtract(theirNumber);
+            int Multiplication = math.Multiply(theirNumber);
 
             Console.WriteLine(Addition);
             Console.WriteLine(Subtraction);

843a2a346326c5bb4f3116397e598dd12e830025
Connor Eliason <connor.eliason@gmail.com>
2022-06-08 15:58:56 -0600

    ...

---
+++ Basic_C#_Programs/mainMethod/mainMethod/Math.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace mainMethod
+{
+    public class Math
+    {
+        public int myMethod(int theirNumber, int secretNumber)
+        {
+            int newNumber = theirNumber + secretNumber;
+            return newNumber;
+        }
+
+        public int myMethod(decimal theirDecimal, decimal secretDecimal)
+        {
+            decimal newDecimal = theirDecimal - secretDecimal;
+            int result = Convert.ToInt32(newDecimal);
+            return result;
+        }
+
+        public int myMethod(string theirString)
+        {
+            int num = Convert.ToInt32(theirString);
+            int result = num + 5;
+            return result;
+        }
+    }
+}
---
+++ Basic_C#_Programs/mainMethod/mainMethod/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace mainMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Pick a number between 1 and 100!");
+            int theirNumber = Convert.ToInt32(Console.ReadLine());
+            int secretNumber = 4;
+
+            Math math = new Math();
+
+            int Addition = math.myMethod(theirNumber, secretNumber);
+            Console.WriteLine(Addition);
+
+            Console.WriteLine("Pick a decimal between 2 and 3");
+            decimal theirDecimal = Convert.ToDecimal(Console.ReadLine());
+            decimal secretDecimal = 5;
+
+            int Subtraction = math.myMethod(theirDecimal, secretDecimal);
+            Console.WriteLine(Subtraction);
+
+            Console.WriteLine("Enter a number:");
+            string theirString = Console.ReadLine();
+            string secretString = "10";
+
+            int Multiplication = math.myMethod(theirString);
+
+            Console.Write(Multiplication);
+            
+            Console.ReadLine();
+        }
+    }
+}

770a4e4f05beadf18610a772fd2acae699565680
Connor Eliason <connor.eliason@gmail.com>
2022-06-08 23:26:05 -0600

    ...

---
+++ Basic_C#_Programs/ClassesAssignment/ClassesAssignment/Math.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassesAssignment
+{
+    public class Math
+    {
+        public int myMethod(int num)
+        {
+            int result = num / 2;
+            return result;
+        }
+
+        public decimal myMethod(decimal num2)
+        {
+            decimal multiplication = num2 * 2;
+            return multiplication;
+        }
+
+        public static double myMethod(double num3)
+        {
+            double addition = num3 + 5;
+            return addition;
+        }
+
+        void myMethod(out int answer, out string message, out string stillNull)
+        {
+            answer = 44;
+            message = "I've been returned";
+            stillNull = null;
+        }
+    }
+}
---
+++ Basic_C#_Programs/ClassesAssignment/ClassesAssignment/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ClassesAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Math math = new Math();
+
+            Console.WriteLine("Enter a number: ");
+            int num = Convert.ToInt32(Console.ReadLine());
+            int division = math.myMethod(num);
+            Console.WriteLine(num + " divided by 2 equals " + division);
+        }
+    }
+}

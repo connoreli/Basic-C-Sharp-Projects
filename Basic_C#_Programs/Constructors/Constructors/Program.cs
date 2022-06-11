430795341f0ccebfa59af5eee8e6299bdad85b1a
Connor Eliason <connor.eliason@gmail.com>
2022-06-11 02:15:20 -0600

    ...

---
+++ Basic_C#_Programs/Constructors/Constructors/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Constructors
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+
+        class constructorChain
+        {
+            private const int x;
+            private var j;
+
+            public constructorChain() : this(10, 55)
+            {
+
+            }
+        }
+    }
+}
